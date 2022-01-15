cd45c3351905c0fbb99510ad956b45b87f04df54
delwinmachan <68820194+delwinmachan@users.noreply.github.com>
2022-01-15 22:41:02 +0530

    updated ejs and admin dashboard

--- index.js
+++ index.js
@@ -2,18 +2,21 @@ var express=require("express");
 var bodyParser=require("body-parser");
   
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/gfg');
+const  ObjectId = require('mongodb').ObjectId;
+const { request } = require("express");
+mongoose.connect('mongodb+srv://Team2022:ddfgiks123@cluster0.0poxf.mongodb.net/gfg?retryWrites=true');
 var db=mongoose.connection;
 db.on('error', console.log.bind(console, "connection error"));
 db.once('open', function(callback){
     console.log("connection succeeded");
 })
-  
+
 var app=express()
-  
-  
+var na="hello";
+app.set('view engine','ejs');  
 app.use(bodyParser.json());
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public/'));
+
 app.use(bodyParser.urlencoded({
     extended: true
 }));
@@ -78,15 +81,111 @@ app.post('/login',function(req,res){
         });
     });
 
-
+    app.get('/Admin_Customers', function(req, res) {
+        
+        console.log(na);
+        db.collection('details').find().toArray(function(err, items) {
+            if(err) throw err;    
+           else{
+                
+                 res.render('Admin_Customers',{
+                    user:items })
+    
+                }           
+            });
+      
+    })
   
   
-app.get('/',function(req,res){
+/*app.get('/',function(req,res){
 res.set({
     'Access-control-Allow-Origin': '*'
     });
 return res.redirect('registration.html');
-}).listen(3000)
+}).listen(3000)*/
+app.get('/', function(req, res) {
+  
+    res.render('index');
+  }).listen(3000);
+  app.get('/about', function(req, res) {
+  
+    res.render('about');
+  });
+  app.post('/Admin_Customers', function(req, res) {
+    var id=req.body.id;
+    console.log(id);
+    var name = req.body.name;
+    var email =req.body.email;
+    var pass = req.body.password;
+    var phone =req.body.phone;
+    var organization = req.body.organization;
+    var aadhar=req.body.aadhar;
+    var address = req.body.address;
+    var country = req.body.country;
+    var pincode = req.body.pincode;
+    var role = req.body.role;
+    res.render('update',{
+        id:id,
+        name:name,
+        email:email,
+        pass:pass,
+        phone:phone,
+        organization:organization,
+        aadharNum:aadhar,
+        address:address,
+        country:country,
+        pincode:pincode,
+        role:role
+    });
+  });
+  /*app.get('/update', function(req, res) {
+     console.log(na);
+    res.render('update',{
+        name:"hello"
+    });
+  });*/
+  app.get('/registration', function(req, res) {
+    
+    res.render('registration');
+  });
+  app.get('/login', function(req, res) {
+  
+    res.render('login');
+  });
+  app.get('/registration', function(req, res) {
+  
+    res.render('registration');
+  });
+  app.post('/update', function(req, res) {
+    var data={
+     name : req.body.name,
+     email:req.body.email,
+     phone:req.body.phone,
+     aadharNum:req.body.aadharNum,
+     address : req.body.address,
+     country :req.body.country,
+    }  
+    var id=req.body.id;
+    db.collection('details').updateOne({"_id": ObjectId(id)},{$set:data},function(err, collection){
+        if (err) throw err;
+        console.log("Record updated Successfully");
+              
+    });
+     
+  });
+ 
+  app.post('/delete', function(req, res) {
+    
+    var id=req.body.id;
+    console.log(id)
+    db.collection('details').deleteOne({"_id": ObjectId(id)},function(err, collection){
+        if (err) throw err;
+        console.log("Record deleted Successfully");
+              
+    });
+     
+  });
+
   
   
 console.log("server listening at port 3000");
